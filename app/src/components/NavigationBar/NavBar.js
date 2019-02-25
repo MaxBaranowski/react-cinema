cbeb7c17762535b1f8901f1ec6d3a2c26738c85c
Max Baranowski <maksim.baranowski@unilinkcash.pl>
2019-02-25 17:14:16 +0100

    navbar in progress

--- app/src/components/NavigationBar/NavBar.js
+++ app/src/components/NavigationBar/NavBar.js
@@ -3,9 +3,27 @@ import { Link } from "react-router-dom";
 import "./styles.scss";
 
 export default class NavBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showResults: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({ showResults: !this.state.showResults });
+  }
+
+  getState() {
+    return this.state.showResults;
+  }
+
   render() {
+    console.log(this.state.showResults)
     return (
-      <aside className="navigation" >
+      < aside className="navigation" >
         <nav>
           <ul>
             <li>
@@ -15,8 +33,10 @@ export default class NavBar extends Component {
               <Link to={`/login`}>Login</Link>
             </li>
           </ul>
-        </nav>
-      </aside>
+        </nav >
+        <button onClick={this.toggle}>Click</button>
+      </aside >
+
     )
   }
 }
