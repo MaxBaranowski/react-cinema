8af74b28b05c168c3573949d171c2e4e763f2890
Max Baranowski <maksim.baranowski@unilinkcash.pl>
2019-03-08 16:36:30 +0100

    adding home template

--- app/src/components/Pages/Home/index.js
+++ app/src/components/Pages/Home/index.js
@@ -13,19 +13,40 @@ export default class Home extends Component {
 
   render() {
     return (
-      <div>
+      <React.Fragment>
         <h1>Home</h1>
-        <div className="grid-wrapper">
-          <div className="item1">1</div>
-          <div className="item2"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque dolores, laborum adipisci quod voluptas itaque cupiditate accusamus quisquam accusantium explicabo porro tenetur hic ea ducimus commodi, expedita similique vitae nihil qui nemo? Culpa omnis velit explicabo dolorum. Nihil rerum placeat eligendi accusamus dolores nisi. Sint sed nihil necessitatibus quos est.</p></div>
-          <div className="item3">3</div>
-          <div className="item4">4</div>
-          <div className="item5">5</div>
-          <div className="item6">6</div>
-          <div className="item7">7</div>
+        <div className="home-wrapper">
+          <header className="home-banner">1</header>
+          <main className="home-sections">
+            <section className="section just-added">
+              <header>
+                <h1>Just Added</h1>
+              </header>
+              <div className="section-body">
+                <div className="item">2_1</div>
+                <div className="item">2_2</div>
+                <div className="item">2_3</div>
+                <div className="item">2_4</div>
+                <div className="item">2_5</div>
+                <div className="item">2_6</div>
+              </div>
+            </section>
+            <section className="section recommended">
+              <header>
+                <h1>Recommended</h1>
+              </header>
+              <div className="section-body">
+                <div className="item">3_1</div>
+                <div className="item">3_2</div>
+                <div className="item">3_3</div>
+                <div className="item">3_4</div>
+                <div className="item">3_5</div>
+                <div className="item">3_6</div>
+              </div>
+            </section>
+          </main>
         </div>
-        {/* {store.getState().showMenu.tos} */}
-      </div >
+      </React.Fragment >
     )
   }
 }
