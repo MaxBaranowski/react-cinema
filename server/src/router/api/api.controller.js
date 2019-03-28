3a31695b97b11bfd9d8e902c77c6f8712cabfe0d
Max Baranowski <maksim.baranowski@unilinkcash.pl>
2019-03-28 12:38:56 +0100

    up

--- server/src/router/api/api.controller.js
+++ server/src/router/api/api.controller.js
@@ -1,6 +1,6 @@
 import DB from "../../models/Database";
-import {MovieFull} from "./models/movieFull"
-import {MovieShort} from "./models/MovieShort"
+import { MovieFull } from "./models/movieFull"
+import { MovieShort } from "./models/MovieShort"
 
 import fs from "fs";
 import axios from "axios";
@@ -8,92 +8,92 @@ import axios from "axios";
 export default class API {
   constructor() {
   }
-  
+
   getMovie = async (req, res, next) => {
     try {
       const {
         id: movieId = "", //5c6178f4b0bba10d976f1220
       } = req.body;
-      
-      await new DB().findById({"schema": Customer, "id": movieId}).then(
+
+      await new DB().findById({ "schema": Customer, "id": movieId }).then(
         (result) => {
           res.json(result)
         }
       ).catch((err) => {
         return next(err);
       });
-      
+
     } catch (err) {
       return next(err);
     }
   };
-  
+
   getMoviesByName = async (req, res, next) => {
     try {
       const {
         name: movieName = "",
       } = req.body;
-      
-      await new DB().findByParams({"schema": Customer, "findKey": "name.first", "findValue": movieName}).then(
+
+      await new DB().findByParams({ "schema": Customer, "findKey": "name.first", "findValue": movieName }).then(
         (result) => {
           res.json(result)
         }
       ).catch((err) => {
         return next(err);
       });
-      
+
     } catch (err) {
       return next(err);
     }
   };
-  
-  
+
+
   getData = async (req, res, next) => {
     let data3 = [];
-    
+
     let time = new Date();
     new Promise((resolve, reject) => {
       fs.readFile('./movies.json', (err, data) => {
         if (err) reject(err);
-        resolve(JSON.parse(data));
+        let filtredDate = [];
+        for (var movie of JSON.parse(data)) {
+          if (Number(movie["year"]) >= 1979) {
+            filtredDate.push(movie)
+          }
+        }
+        resolve(filtredDate);
       });
     }).then((data) => {
-      // let data2 = data.slice(0, 3);
       let i = 100;
-  
-      for (let movie of data) {
-        i += 300;
-        (function(movie,i) {
+      for (let movie of data.slice(4005, 4500)) {
+        i += 100;
+        (function (movie, i) {
           setTimeout(function () {
-            let url = `http://www.omdbapi.com/?t=${movie.title.split(" ").join("+")}&y=${movie.year}&plot=full&apikey=e5c95e8c`;
+            //1 key: e5c95e8c
+            //2 key: 435a86bf
+            //3 key: BanMePlz 
+            let url = `http://www.omdbapi.com/?t=${movie.title.split(" ").join("+")}&y=${movie.year}&plot=full&apikey=435a86bf`;
             axios(url)
               .then((result) => {
                 let movieFull = result.data;
-                if (!movieFull.hasOwnProperty("Title")) {
-                  return;
+                if (movieFull.hasOwnProperty("Title") && movieFull.Poster.length > 5) {
+                  new DB().fillCollection({ "schema": MovieShort, "data": movieFull }).then(
+                    (result) => {
+                    }
+                  ).catch((err) => {
+                  });
+
+                  new DB().fillCollection({ "schema": MovieFull, "data": movieFull }).then(
+                    (result) => {
+                    }
+                  ).catch((err) => {
+                  });
                 }
-                // data3.push(movieFull)
-        
-                new DB().fillCollection({"schema": MovieShort, "data": movieFull}).then(
-                  (result) => {
-                  }
-                ).catch((err) => {
-                  return next(err);
-                });
-        
-                new DB().fillCollection({"schema": MovieFull, "data": movieFull}).then(
-                  (result) => {
-                  }
-                ).catch((err) => {
-                  return next(err);
-                });
-        
-        
-                return result; // return array with objects
-              })
+                console.count()
+              }).catch(() => { })
           }, i);
-        })(movie,i)
-        
+        })(movie, i)
+
       }
     }).then(() => {
       function millisToMinutesAndSeconds(millis) {
@@ -101,26 +101,13 @@ export default class API {
         var seconds = ((millis % 60000) / 1000).toFixed(0);
         return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
       }
-      
-      
-      // new DB().fillCollection({"schema": MovieFull, "data": data3}).then(
-      //   (result) => {
-      //   }
-      // ).catch((err) => {
-      //   return next(err);
-      // });
-      // console.log(data3.length)
       res.send(`done, time: ${millisToMinutesAndSeconds(new Date() - time)}ms`);
     });
-    // fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-    //   if (err) throw err;
-    //   console.log('Saved!');
-    // });
   };
-  
-  
+
+
   index = (req, res, next) => {
     res.status(200).render("api");
   }
-  
+
 }
