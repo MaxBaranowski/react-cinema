44982d585e15ae01284cf1a51fc260737b5232cf
Max Baranowski <max.baranowski@mail.ru>
2019-04-19 23:38:59 +0200

    search in progress

--- app/src/components/Pages/components/SearchPanel/index.js
+++ app/src/components/Pages/components/SearchPanel/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import Icon from "./components/Icon";
 import SearchField from "./components/Search";
@@ -10,32 +10,58 @@ export default class SearchPanel extends Component {
     this.state = {
       searchResults: []
     };
-
+    
     this.SearchBar = React.createRef();
-
+    
     this.handleSearchOnFocus = () => {
       this.SearchBar.current.classList.toggle("searchBar-active");
+      console.log(this.SearchBar.current.classList)
     };
-
+    
     this.searchResults = data => {
       this.setState({
         searchResults: data
       });
     };
+    
+    this.makeResults = (movies) => {
+      if(movies.length < 1){
+        return [];
+      }
+      let comp = [];
+      for (let movie of movies) {
+        comp.push(
+          <div className="search-results-item">
+            <img src={movie.Poster} alt=""/>
+            <div className="search-result-description">
+              <h1 className="title">{movie.Title}</h1>
+              <p className="released">{movie.Released}</p>
+              <p className="genre">{movie.Genre}</p>
+              <p className="rating">{movie.Ratings[0].Value}</p>
+            </div>
+          </div>
+        );
+      }
+      return comp;
+    }
   }
-
+  
   render() {
-    const { searchResults } = this.state;
+    const {searchResults} = this.state;
     return (
-      <div className="searchBar" ref={this.SearchBar}>
-        <Icon />
-        <SearchField
-          handlerFocus={this.handleSearchOnFocus}
-          handlerBlur={this.handleSearchOnFocus}
-          returnSearchResultsToParent={this.searchResults}
-        />
-        <div className="search-results">{JSON.stringify(searchResults)}</div>
-      </div>
+      <section className="searchBar">
+        <main ref={this.SearchBar}>
+          <Icon/>
+          <SearchField
+            handlerFocus={this.handleSearchOnFocus}
+            handlerBlur={this.handleSearchOnFocus}
+            returnSearchResultsToParent={this.searchResults}
+          />
+        </main>
+        <aside className="search-results">
+          {this.makeResults(searchResults)}
+        </aside>
+      </section>
     );
   }
 }
