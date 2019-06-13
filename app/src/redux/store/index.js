eca6650c56e2f237c01c81015a839e961410eb2f
Max Baranowski <maksim.baranowski@unilinkcash.pl>
2019-06-13 16:24:54 +0200

    developers tools +  thunk add

--- app/src/redux/store/index.js
+++ app/src/redux/store/index.js
@@ -1,9 +1,13 @@
-import {createStore} from "redux";
+import { createStore, applyMiddleware } from "redux";
+
+import thunk from "redux-thunk";
 import reducer from "../reducers";
+
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const state = {
   background: "",
-  showMenu: false // show hide menu 
+  showMenu: false // show hide menu
 };
 
-export const store = createStore(reducer, state);
+export const store = createStore(reducer, state,  composeWithDevTools(applyMiddleware(thunk)));
